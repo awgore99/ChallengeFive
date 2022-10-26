37b3ccee60b6fe754b1360c140a4c29327a8b533
awgore99 <awgore99@gmail.com>
2022-10-26 11:40:01 -0500

    Added function getCurrentDate to display the current date in the header

---
+++ script.js
@@ -0,0 +1,122 @@
+// Array of objects to represent each hour of the day to iterate through
+var myDay = [
+    {
+        id: "0",
+        hour: "06",
+        time: "06",
+        am_pm: "am",
+        reminder: ""
+    },
+    {
+        id: "1",
+        hour: "07",
+        time: "07",
+        am_pm: "am",
+        reminder: ""
+    },
+    {
+        id: "2",
+        hour: "08",
+        time: "08",
+        am_pm: "am",
+        reminder: ""
+    },
+    {
+        id: "3",
+        hour: "09",
+        time: "09",
+        am_pm: "am",
+        reminder: ""
+    },
+    {
+        id: "4",
+        hour: "10",
+        time: "10",
+        am_pm: "am",
+        reminder: ""
+    },
+    {
+        id: "5",
+        hour: "11",
+        time: "11",
+        am_pm: "am",
+        reminder: ""
+    },
+    {
+        id: "6",
+        hour: "12",
+        time: "12",
+        am_pm: "pm",
+        reminder: ""
+    },
+    {
+        id: "7",
+        hour: "01",
+        time: "13",
+        am_pm: "pm",
+        reminder: ""
+    },
+    {
+        id: "8",
+        hour: "02",
+        time: "14",
+        am_pm: "pm",
+        reminder: ""
+    },
+    {
+        id: "9",
+        hour: "03",
+        time: "15",
+        am_pm: "pm",
+        reminder: ""
+    },
+    {
+        id: "10",
+        hour: "04",
+        time: "16",
+        am_pm: "pm",
+        reminder: ""
+    },
+    {
+        id: "11",
+        hour: "05",
+        time: "17",
+        am_pm: "pm",
+        reminder: ""
+    },
+    {
+        id: "12",
+        hour: "06",
+        time: "18",
+        am_pm: "pm",
+        reminder: ""
+    },
+    {
+        id: "13",
+        hour: "07",
+        time: "19",
+        am_pm: "pm",
+        reminder: ""
+    },
+    {
+        id: "14",
+        hour: "08",
+        time: "20",
+        am_pm: "pm",
+        reminder: ""
+    },
+    {
+        id: "15",
+        hour: "09",
+        time: "21",
+        am_pm: "pm",
+        reminder: ""
+    },
+    
+]
+
+// gets the current date to append to the top of the page
+function getCurrentDate() {
+    var currentDate = moment().format('dddd, MMMM Do');
+    $("#currentDay").text(currentDate);
+}
