73ba826b45317fa662eb3fa5b469acf6625e98ff
awgore99 <awgore99@gmail.com>
2022-10-27 12:33:01 -0500

    Added runReminders() call, and moved myDay to its own file to clean up the code

--- script.js
+++ script.js
@@ -1,119 +1,5 @@
 // Array of objects to represent each hour of the day to iterate through
-var myDay = [
-    {
-        id: "0",
-        hour: "06",
-        time: "06",
-        am_pm: "am",
-        reminder: ""
-    },
-    {
-        id: "1",
-        hour: "07",
-        time: "07",
-        am_pm: "am",
-        reminder: ""
-    },
-    {
-        id: "2",
-        hour: "08",
-        time: "08",
-        am_pm: "am",
-        reminder: ""
-    },
-    {
-        id: "3",
-        hour: "09",
-        time: "09",
-        am_pm: "am",
-        reminder: ""
-    },
-    {
-        id: "4",
-        hour: "10",
-        time: "10",
-        am_pm: "am",
-        reminder: ""
-    },
-    {
-        id: "5",
-        hour: "11",
-        time: "11",
-        am_pm: "am",
-        reminder: ""
-    },
-    {
-        id: "6",
-        hour: "12",
-        time: "12",
-        am_pm: "pm",
-        reminder: ""
-    },
-    {
-        id: "7",
-        hour: "01",
-        time: "13",
-        am_pm: "pm",
-        reminder: ""
-    },
-    {
-        id: "8",
-        hour: "02",
-        time: "14",
-        am_pm: "pm",
-        reminder: ""
-    },
-    {
-        id: "9",
-        hour: "03",
-        time: "15",
-        am_pm: "pm",
-        reminder: ""
-    },
-    {
-        id: "10",
-        hour: "04",
-        time: "16",
-        am_pm: "pm",
-        reminder: ""
-    },
-    {
-        id: "11",
-        hour: "05",
-        time: "17",
-        am_pm: "pm",
-        reminder: ""
-    },
-    {
-        id: "12",
-        hour: "06",
-        time: "18",
-        am_pm: "pm",
-        reminder: ""
-    },
-    {
-        id: "13",
-        hour: "07",
-        time: "19",
-        am_pm: "pm",
-        reminder: ""
-    },
-    {
-        id: "14",
-        hour: "08",
-        time: "20",
-        am_pm: "pm",
-        reminder: ""
-    },
-    {
-        id: "15",
-        hour: "09",
-        time: "21",
-        am_pm: "pm",
-        reminder: ""
-    },
-    
-]
+import myDay from 'myDayArray.js';
 
 // gets the current date to append to the top of the page
 function getCurrentDate() {
@@ -186,3 +72,15 @@ myDay.forEach(function(selectedHour) {
             "class": "future"
         })
     }
+    // creates save button for each element of the grid
+    var saveButton = $("<i class='far fa-save fa-lg'></i>")
+    var saveReminder = $("<button>")
+        .attr({
+            "class": "col-md-1 saveBtn"
+    });
+    saveReminder.append(saveButton);
+    hourRow.append(hourField, hourReminder, saveReminder);
+})
+
+runReminders();
+
